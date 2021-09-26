ebe1b1f1bc580166266ec02f9f3d8efbd5456393
Islam <alaev095@bk.ru>
2021-09-26 15:41:34 +0300

    test2

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+document.addEventListener('touchstart', handleTouchStart, false)
+document.addEventListener('touchmove', handleTouchMove, false)
+
+let x = null
+let y = null
+
+function handleTouchStart (event) {
+  const firstTouch = event.touches[0]
+  console.log(firstTouch)
+  x = firstTouch.clientX
+}
+
+function handleTouchMove (event) {
+  let x2 = event.touches[0].clientX
+  let xDiff = x2 - x
+  if (xDiff > 0) {
+    window.scrollBy(0,50)
+    console.log('right')
+  } else {
+    console.log('left')
+  }
+}
+
 function animation () {
+  window.scrollBy(0,50)
   let img1 = document.getElementById('layer_4')
   console.log('slam')
   let position = 228
